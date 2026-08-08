961b4c226e3996a56710750e874e8f6dde417d9e
luisy <pogdopogdopogchamp@gmail.com>
2026-08-08 18:09:29 -0300

    feat(plugins): add PLUGIN_DESC to plugin_test and plugin_test2

--- src/plugins/plugin_test.py
+++ src/plugins/plugin_test.py
@@ -1,3 +1,5 @@
 
+PLUGIN_DESC = "Plugin de teste 1"
+
 def test_print():
     print("Reproduce test")
--- src/plugins/plugin_test2.py
+++ src/plugins/plugin_test2.py
@@ -1,3 +1,5 @@
 
+PLUGIN_DESC = "Plugin de teste 2"
+
 def test_print2():
     print("Reproduce test2")
